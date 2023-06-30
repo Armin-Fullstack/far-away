830320b916e9e893b052f88aa4389c69420113af
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-06-30 20:49:11 +0330

    feat: rendered InitialItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
+];
+interface PackingItemProps {
+  packObj: {
+    id: number;
+    description: string;
+    quantity: number;
+    packed: boolean;
+  };
+}
 function App(): JSX.Element {
   return (
     <div className="app">
@@ -22,12 +35,29 @@ function Form(): JSX.Element {
 }
 
 function PackingLists(): JSX.Element {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((packingItem) => {
+          return <PackingItem packObj={packingItem} key={packingItem.id} />;
+        })}
+      </ul>
+    </div>
+  );
 }
-
+function PackingItem({ packObj }: PackingItemProps): JSX.Element {
+  return (
+    <li>
+      <span style={packObj.packed ? {textDecoration: "line-through"} : {}}>
+        {packObj.quantity} {packObj.description}
+      </span>
+      <button>✖️</button>
+    </li>
+  );
+}
 function Stats(): JSX.Element {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have x items on your list, and you already packed x (x%)</em>
     </footer>
   );
