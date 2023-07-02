1713f3e1b59a2cce5f637281b6fa77d49f542a45
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-07-02 15:18:06 +0330

    feat: implemented Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { SyntheticEvent, useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "charger", quantity: 1, packed: false },
-];
 interface PackingItemProps {
   packObj: {
-    id: number;
+    id: string;
     description: string;
     quantity: number;
     packed: boolean;
@@ -19,12 +14,22 @@ interface Pack {
   packed: boolean;
   id: string;
 }
+interface FormProps {
+  onAddPack: (packItem: Pack) => void
+}
+interface PackingListsProps {
+  pack: Pack[]
+}
 function App(): JSX.Element {
+  const [pack , setPack] = useState<Pack[]>([])
+  function handleAddPackItems(packItem: Pack): void {
+    setPack(pack => [...pack , packItem]) // current new item of array + new added item to array => new state depends on current state 
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingLists />
+      <Form onAddPack={handleAddPackItems}/>
+      <PackingLists pack={pack}/>
       <Stats />
     </div>
   );
@@ -34,39 +39,39 @@ function Logo(): JSX.Element {
   return <h1>🏝 Far Away 💼</h1>;
 }
 
-function Form(): JSX.Element {
+function Form({onAddPack}: FormProps): JSX.Element {
   const [description , setDescription] = useState<string>("")
   const [quantity , setQuantity] = useState<number>(1)
+  
   function handleSubmit(e: SyntheticEvent) {
     e.preventDefault()
     if(!description) return
     const newPack: Pack = {description, quantity, packed: false, id: crypto.randomUUID()}
-    console.log(newPack); // We wanna pass data from Form to PackingList but they are siblings(thinking about state and state management)
-
+    onAddPack(newPack)
     setDescription("")
     setQuantity(1)
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need to your 😍 trip?</h3>
-      <select value={quantity} onChange={e => setQuantity(+e.target.value)}>
+      <select value={quantity} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." value={description} onChange={e => setDescription(e.target.value)}/>
+      <input type="text" placeholder="item..." value={description} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value)}/>
       <button>ADD</button>
     </form>
   );
 }
 
-function PackingLists(): JSX.Element {
+function PackingLists({pack}: PackingListsProps): JSX.Element {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((packingItem) => {
+        {pack.map((packingItem) => {
           return <PackingItem packObj={packingItem} key={packingItem.id} />;
         })}
       </ul>
