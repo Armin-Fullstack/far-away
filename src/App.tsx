566def43629447fc4198ee14c440c8cf6a1a4805
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-07-03 17:42:42 +0330

    feat: implemented Stats component

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App(): JSX.Element {
         onDeletePack={handleDeletePack}
         onTogglePack={handleTogglePack}
       />
-      <Stats />
+      <Stats pack={pack} />
     </div>
   );
 }
@@ -149,10 +149,24 @@ function PackingItem({
     </li>
   );
 }
-function Stats(): JSX.Element {
+function Stats({ pack }: PackingListsProps): JSX.Element {
+  if (!pack.length)
+    // conditional rendering(return)
+    return (
+      <footer className="stats">
+        <p>Start adding some items to your packing list 🚀</p>
+      </footer>
+    );
+  const packItems = pack.length; // we are Driving the state bc these data completely depends on pack state
+  const packedLength = pack.filter((element) => element.packed).length;
+  const percentage = Math.round((packedLength / packItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have x items on your list, and you already packed x (x%)</em>
+      <em>
+        {percentage !== 100
+          ? `💼You have ${packItems} items on your list, and you already packed ${packedLength} (${percentage}%)`
+          : "You got everything! Ready to go ✈️"}
+      </em>
     </footer>
   );
 }
